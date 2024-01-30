22c204742e2455cebcbf7740e4bed3cc076bd6ac
Stefan Zemljic <stefan.zemljic@protonmail.com>
2024-01-30 19:25:49 +0100

    Make -y work for ./run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t piper-tts . &&
-docker run --rm -it -v "C:\\Users\\Stefan\\repos\\piper":/root/workdir piper-tts bash ./convert.sh input/$1
+docker run --rm -it -v "C:\\Users\\Stefan\\repos\\piper":/root/workdir piper-tts bash ./convert.sh $@
